1d891b4878f5686e99f22a6ce1be4848125d605f
Alireza.M.H.N <126543376+AlirezaMohyyaldin@users.noreply.github.com>
2023-06-24 20:48:22 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projehaybarname
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double midterm = Convert.ToDouble(Console.ReadLine()); 
+            double final = Convert.ToDouble(Console.ReadLine());
+            double show = calc(midterm, final);
+            Console.WriteLine(show);
+        }
+       public static double calc(double x,double y)
+        {
+            double resault = 0.35 * x + 0.65 * y;
+            return resault;
+        }
+    }
+}
